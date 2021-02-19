511950031c413fa87205a75134d92479e20cbd3d
slicemedia <56181188+slicemedia@users.noreply.github.com>
2021-02-19 07:43:22 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -8,12 +8,12 @@ function checkMenuState() {
     var navMenu = document.getElementById("navigation-menu-mobile");
     var navMenuIcon = document.getElementById("hamburger_btn")
     var menuState = navMenu.getAttribute("data-menuOpen");
-    if (menuState == "open" && isMobile < 770 ) {
+    if (menuState == "open" && isMobile < 1025 ) {
         navMenu.setAttribute("data-menuOpen", "closed");
         navMenu.style.display = "none";
         navMenuIcon.src="./icons und Logos/sevent-sandwich.svg";
         console.log("menu-closed");
-    } else if (menuState == "closed" && isMobile < 770 ) {
+    } else if (menuState == "closed" && isMobile < 1025 ) {
         navMenu.style.display = "flex";
         navMenu.setAttribute("data-menuOpen", "open");
         navMenuIcon.src="./icons und Logos/close.svg";
