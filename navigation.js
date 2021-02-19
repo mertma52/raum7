6f42bd857ffa7ec37985a2c76323221f530f6028
slicemedia <56181188+slicemedia@users.noreply.github.com>
2021-02-19 07:32:49 +0100

    new stuff

--- navigation.js
+++ navigation.js
@@ -2,14 +2,17 @@
 
 function checkMenuState() {
     var navMenu = document.getElementById("navigation-menu-mobile");
+    var navMenuIcon = document.getElementById("hamburger_btn")
     var menuState = navMenu.getAttribute("data-menuOpen");
     if (menuState == "open") {
         navMenu.setAttribute("data-menuOpen", "closed");
         navMenu.style.display = "none";
+        navMenuIcon.src="./icons und Logos/sevent-sandwich.svg";
         console.log("menu-closed");
     } else if (menuState == "closed") {
         navMenu.style.display = "flex";
         navMenu.setAttribute("data-menuOpen", "open");
+        navMenuIcon.src="./icons und Logos/close.svg";
         console.log("menu-open");
     }
 }
